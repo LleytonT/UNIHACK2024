41357cd99385d53969c8511787070a88213a37f7
Lleyton <z5316214@ad.unsw.edu.au>
2024-03-03 14:41:19 +1100

    Rebased

--- frontend/webapp/src/components/Summariser.jsx
+++ frontend/webapp/src/components/Summariser.jsx
@@ -9,8 +9,6 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import LogoutIcon from '@mui/icons-material/Logout';
 import AddIcon from '@mui/icons-material/Add';
 import { useNavigate } from "react-router-dom";
